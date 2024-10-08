73dde0faeac44c4ee0c67510eb28957b5325d533
Alecsandro05 <alecsandrosales1@gmail.com>
2024-10-08 17:46:29 -0300

    Created function copyClipPasword

--- src/App.jsx
+++ src/App.jsx
@@ -15,25 +15,37 @@ function App() {
       console.log(newPassword)
     }
     setPassword(newPassword)
+    setCopyPassword("Copiar")
   }
-
+  const copyClipPassword = () => {
+    const teste = window.navigator.clipboard.writeText(password)
+    teste.then(() => setCopyPassword("Copiado!"))
+  }
   return (
     <div className="w-screen h-screen bg-zinc-900">
       <main className="flex justify-center items-center h-screen flex-col">
-        <h1 className="text-zinc-100 font-bold text-6xl ">GERADOR DE SENHAS</h1>
+        <h1 className=" font-bold text-6xl bg-gradient-to-r from-red-500 to-orange-300 bg-clip-text text-transparent">
+          GERADOR DE SENHAS
+        </h1>
         <div className="flex gap-6 py-10">
           <button
             onClick={() => generatePassword()}
             className="border-2 rounded-md text-lg py-2 px-4 text-zinc-50 font-semibold  duration-500 hover:border-cyan-800"
           >
-            Gerar!
+            Gerar !
           </button>
-          <button className="border-2 rounded-md text-lg py-2 px-4 text-zinc-50 font-semibold duration-500 hover:border-green-700">
+          <button
+            onClick={() => copyClipPassword()}
+            className="border-2 rounded-md text-lg py-2 px-4 text-zinc-50 font-semibold duration-500 hover:border-green-700"
+          >
             {copyPassword}
           </button>
         </div>
         <div>
-          <span className="text-zinc-50 text-lg cursor-pointer">
+          <span
+            onClick={() => copyClipPassword()}
+            className="text-zinc-50 text-lg cursor-pointer relative duration-2000 hover:bottom-2 hover:text-green-400"
+          >
             {password}
           </span>
         </div>
